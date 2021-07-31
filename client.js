72bb1fd7596153c671282096870d316d4e0006dd
SHS666638 <85347510+SHS666638@users.noreply.github.com>
2021-07-31 17:19:05 +0700

    Create client.js

---
+++ client.js
@@ -0,0 +1,12 @@
+var name = HR3
+var client = client.sudo().queue
+
+
+let client.server = {
+    client.ip=emplayer.com /*Not Change*/
+    client.port=8080 /*Not Change*/
+var client = client.server
+}
+var sudo = [0]
+var num=[1]
+num[1,2]
